c8a14e1fa6c75fb643744e7bd59c52e3046f24ec
Bruno Alves <hello@brunoalves.app>
2022-01-27 19:25:56 +0000

    global styles added

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,15 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   :root {
+    --red: #e52e4d;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
     --background: #f0f2f5;
+    --shape: #fff;
   }
 
   * {
@@ -11,7 +19,36 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
   body {
     background: var(--background);
+    -webkit-font-smoothing: antialised;
+  }
+
+  body, input, text-area, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 `
