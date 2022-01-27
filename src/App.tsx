24f268296cd4321a69e366d16975cdce55109551
Bruno Alves <hello@brunoalves.app>
2022-01-27 20:36:02 +0000

    front end added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from './components/Header'
+import { Dashboard } from './components/Dashboard'
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Logo</h1>
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+    </>
   )
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { Container } from './styles'
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Compras de mercado</td>
+            <td className="withdraw">- R$ 500,00</td>
+            <td>Saúde</td>
+            <td>20/06/2020</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Serviço</td>
+            <td>20/06/2020</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 1.100,00</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
