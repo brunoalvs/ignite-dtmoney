91121fb9a4c0621caa33fcbd2daf4ee4f2d55e8e
Bruno Alves <hello@brunoalves.app>
2022-01-28 09:53:15 +0000

    Front end of new transaction form in modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Modal from 'react-modal'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
+import { NewTransactionModal } from './components/NewTransactionModal'
 
 Modal.setAppElement('#root')
 
@@ -21,12 +22,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
     </>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
 
 import { Container, Content } from './styles'
