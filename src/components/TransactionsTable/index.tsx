54d454ca3384633dcde32813f1c39efb1b2a31f9
Bruno Alves <hello@brunoalves.app>
2022-01-28 09:09:40 +0000

    Primeiros componentes da aplicação

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
 import { Container } from './styles'
 
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(() => {
+    api
+      .get('transactions')
+      .then(response => setTransactions(response.data))
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <Container>
       <table>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createServer } from 'miragejs'
 import { App } from './App'
 import { GlobalStyle } from './styles/global'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Compras de mercado',
+          amount: 500,
+          category: 'Food',
+          type: 'withdraw',
+          createdAt: new Date(),
+        },
+        {
+          id: 2,
+          title: 'Desenvolvimento de website',
+          amount: 12000,
+          category: 'Services',
+          type: 'deposit',
+          createdAt: new Date(),
+        },
+      ]
+    })
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
